71f5f544da1926e074d8c1c8cee59d9ea730ac7e
Dhruvisha Patel <110115918+pateld19@users.noreply.github.com>
2023-11-28 23:32:33 -0500

    finally it works

--- list_backup.sh
+++ list_backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # SSH into your EC2 instance and list backup files
-ssh -i /Users/dhruvipatel/Downloads/4240-ec2-backup-main/4240-keypair.pem ec2-user@ec2-3-85-77-13.compute-1.amazonaws.com << EOF
+ssh -i 4240-keypair.pem ec2-user@ec2-54-242-73-140.compute-1.amazonaws.com '
   # Specify the directory where your backup files are located on the EC2 instance
   BACKUP_DIR="/home/ec2-user"  # Update this with the actual path
   
   # List the contents of the backup directory
   echo "Contents of backup directory $BACKUP_DIR:"
   ls -lh "$BACKUP_DIR"
-EOF
+'
