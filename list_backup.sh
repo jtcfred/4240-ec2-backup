2776ea8e268a7ff2f2359e98dc553e8ba496d36f
Dhruvisha Patel <110115918+pateld19@users.noreply.github.com>
2023-11-29 21:04:31 -0500

    Update list_backup.sh should work now

--- list_backup.sh
+++ list_backup.sh
@@ -4,15 +4,18 @@
 <<<<<<< HEAD
 #!/bin/bash
 
-# Load cloud configuration from .cloud-config.conf
 source .cloud-config.conf
 
+
 # SSH into your EC2 instance and list only tar.gz files in the backup directory
-ssh -i "$PEM_FILE" "$DEST_USER@$HOST_SERVER" '
+ssh -i 4240-keypair.pem ec2-user@ec2-54-161-37-214.compute-1.amazonaws.com '
+  # Specify the directory where your backup files are located on the EC2 instance
+  BACKUP_DIR="/home/ec2-user"  # Update this with the actual path
+
   # List only tar.gz files in the backup directory
-  echo "Tar.gz files in backup directory $DEST_DIR:"
-  find "$DEST_DIR" -type f -name "*.tar.gz"
-'
+  echo "Tar.gz files in backup directory $BACKUP_DIR:"
+  find "$BACKUP_DIR" -type f -name "*.tar.gz"
+  '
 =======
 ssh -i /Users/simonhughes/Desktop/CPSC4240/4240-keypair.pem ec2-user@ec2-54-242-73-140.compute-1.amazonaws.com
 # Specify the directory where your backup files are located on the EC2 instance
